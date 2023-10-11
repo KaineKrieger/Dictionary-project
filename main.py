41eb8d8633491c96beccbf6ea5ec32dbd234218f
Horner_Adam <horna733@hsd.k12.or.us>
2023-10-11 10:26:25 -0700

    end of class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 Eldrazi = {"Emrakul" : "the aeons torn" ,
            "Kozliek" : "the great distortion",
-            "Ulamog" : "the Ceaseless Hunger"}
+           "Ulamog" : "the Ceaseless Hunger"}
 
 
 User_interest  = input("which group do you wish to know about? Phyrexians Eldrazi or Gatewatch?: ")
@@ -12,7 +12,13 @@ if User_interest == "Eldrazi":
     key_list = list(Eldrazi.keys())
     key_list = ", ".join(key_list)
     Second_interest = input("which Eldrazi titan do you wish to know about? " + key_list + "? ")
+    if Second_interest == "Emrakul":
+        print(Eldrazi["Emrakul"])
 
+    if Second_interest == "Ulamog":
+        print(Eldrazi["Ulamog"])
 
+    if Second_interest == "Kozilek":
+        print(Eldrazi["Kozilek"])
 
 
