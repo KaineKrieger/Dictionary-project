28d2cda89e7dfe2f5f6566989d7bcc95493787fe
Horner_Adam <horna733@hsd.k12.or.us>
2023-10-16 09:38:00 -0700

    adding lore

---
+++ lore.py
@@ -0,0 +1,50 @@
+
+
+
+Eldrazi = {"Emrakul" : "the aeons torn" ,
+           "Kozilek" : "the great distortion",
+           "Ulamog" : "the Ceaseless Hunger"}
+
+Emrakul = ""
+
+Kozilek = ""
+
+Ulamog = ""
+
+
+Gatewatch = {"Jace Beleren" : "Arcane strategist",
+             "Gideon Jura" : "Champion of Justice",
+             "Chandra Nalar" : "Bold Pyromancer",
+             "Nissa Revnae" : "Natures Artisan",
+             "Liliana Vess" : "Defiant Necromancer"}
+
+Jace = ""
+
+Gideon = ""
+
+Chandra = ""
+
+Nissa = ""
+
+Liliana = ""
+
+
+Phyrexians = {"Elesh Norn" : "Mother of Machines, Grand Cenobite",
+              "Vorinclex" : "Voice of Hunger, Monstrous Raider",
+              "Jin Gitaxis" : "Progress Tyrant, Core Augur",
+              "Urabrask" : "The Hidden, Heretic Praetor",
+              "Sheoldred" : "Whispering one, the Apocaplypse",
+              "Gix" : "Yawgmoth Praetor"}
+
+
+Elesh_Norn = ""
+
+Vorinclex = ""
+
+Jin_Gitaxis = ""
+
+Urabrask = ""
+
+Sheoldred = ""
+
+Gix = ""
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+import lore 
 
 
-Eldrazi = {"Emrakul" : "the aeons torn" ,
-           "Kozliek" : "the great distortion",
-           "Ulamog" : "the Ceaseless Hunger"}
+
+
+
 
 
 User_interest  = input("which group do you wish to know about? Phyrexians Eldrazi or Gatewatch?: ")
 
 
 if User_interest == "Eldrazi":
-    key_list = list(Eldrazi.keys())
+    key_list = list(lore.Eldrazi.keys())
     key_list = ", ".join(key_list)
     Second_interest = input("which Eldrazi titan do you wish to know about? " + key_list + "? ")
     if Second_interest == "Emrakul":
-        print(Eldrazi["Emrakul"])
+        print(lore.Eldrazi["Emrakul"])
 
     if Second_interest == "Ulamog":
-        print(Eldrazi["Ulamog"])
+        print(lore.Eldrazi["Ulamog"])
 
     if Second_interest == "Kozilek":
-        print(Eldrazi["Kozilek"])
+        print(lore.Eldrazi["Kozilek"])
 
 
