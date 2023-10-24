03bf7636e679ce1f5739f06c34b6d910109d67c9
Horner_Adam <horna733@hsd.k12.or.us>
2023-10-24 09:55:02 -0700

    getting help

--- lore.py
+++ lore.py
@@ -1,20 +1,14 @@
 
-
-
-Eldrazi = {"Emrakul" : "the aeons torn" ,
-           "Kozilek" : "the great distortion",
-           "Ulamog" : "the Ceaseless Hunger"}
-
 Emrakul = ("Emrakul is the Titan of Corruption, the largest and most fearsome of the Eldrazi.",
-        "She does not affect inorganic matter - rocks, water, etc.",
-        "- but twists all things living, be they plants, animals, or other sentient beings.",
-        "She causes silent terror wherever she soars, embodying desolation, emotional and physical distance,",
-        "the chill of the void, and the terror of being alone. According to Gideon Jura, she is 150 feet tall.",
-        "The Emrakul lineage is characterized by flesh-like lattice structures, uneasy bilateral symmetry,",
-        " and tentacles that end in knobby, vestigial digits. Looking like spongy, alien baleen,",
-        " the lattice respires independent of the Eldrazi herself, supporting a host of unseen mite creatures within.",
-        " Some within this lineage, including Emrakul herself, float above the ground,",
-        " using unknown powers to alter the surrounding gravity.")
+           "She does not affect inorganic matter - rocks, water, etc.",
+           "- but twists all things living, be they plants, animals, or other sentient beings.",
+           "She causes silent terror wherever she soars, embodying desolation, emotional and physical distance,",
+           "the chill of the void, and the terror of being alone. According to Gideon Jura, she is 150 feet tall.",
+           "The Emrakul lineage is characterized by flesh-like lattice structures, uneasy bilateral symmetry,",
+           " and tentacles that end in knobby, vestigial digits. Looking like spongy, alien baleen,",
+           " the lattice respires independent of the Eldrazi herself, supporting a host of unseen mite creatures within.",
+           " Some within this lineage, including Emrakul herself, float above the ground,",
+           " using unknown powers to alter the surrounding gravity.")
 
 Kozilek = ("Kozilek(KOH-zih-lek), the Butcher of Truth is one of the three Eldrazi titans and",
            " the inspiration for the Zendikari merfolk god Cosi and the Kore god Talib",
@@ -29,18 +23,34 @@ Kozilek = ("Kozilek(KOH-zih-lek), the Butcher of Truth is one of the three Eldra
            " in tough carapaces. Kozilek's drones can absorb life just by their presence but prefer",
            " to rend flesh with their onyx-like projections.")
 
-
-
 Ulamog = ("Ulamog, the 'Infinite Gyre' is one of the three Eldrazi titans",
-        " and the monstrosity whose half-forgotten name came to be known as the Zendikari merfolk sea god Ula.",
-        " Ulamog is the Titan of Consumption, as he and his brood consume living and unliving matter alike.",
-        " In doing so, they drain mana and vital energy from everything in the vicinity.",
-        " His processors transform the consumed energy into spell-like effects that defy",
-        " the understanding of Zendikari mages, creating plagues, deadly parasites, and endless teeming spawn.",
-        " The processors leave distinctive white, chalky lattice-like structures in their wake.",
-        " Everything, including water, is transformed into a white manaless husk of what it once was.",
-        " Ulamog is emblematic of plague, the blind bonds between parasite and host, and overabundance;",
-        " he is creation and destruction wrapped together in unholy harmony.")
+          " and the monstrosity whose half-forgotten name came to be known as the Zendikari merfolk sea god Ula.",
+          " Ulamog is the Titan of Consumption, as he and his brood consume living and unliving matter alike.",
+          " In doing so, they drain mana and vital energy from everything in the vicinity.",
+          " His processors transform the consumed energy into spell-like effects that defy",
+          " the understanding of Zendikari mages, creating plagues, deadly parasites, and endless teeming spawn.",
+          " The processors leave distinctive white, chalky lattice-like structures in their wake.",
+          " Everything, including water, is transformed into a white manaless husk of what it once was.",
+          " Ulamog is emblematic of plague, the blind bonds between parasite and host, and overabundance;",
+          " he is creation and destruction wrapped together in unholy harmony.")
+
+Eldrazi = { "Emrakul" : {
+                    "card": "the aeons torn",
+                    "lore": Emrakul,
+                },
+                "Kozilek" : {
+                        "card": "the great distortion",
+                        "lore": Kozilek,
+                },
+                "Ulamog" : {
+                        "card": "the Ceaseless Hunger",
+                        "lore": Ulamog,
+                },
+        }
+
+
+
+
 
 
 
@@ -50,15 +60,33 @@ Gatewatch = {"Jace Beleren" : "Arcane strategist",
              "Nissa Revnae" : "Natures Artisan",
              "Liliana Vess" : "Defiant Necromancer"}
 
-Jace = ""
+Jace = ("Jace Beleren is a compleated human planeswalker and mind-mage from Vryn.",
+"While working with the Infinite Consortium on Ravnica, he went by the name Berrim.",
+"He is the former Living Guildpact of Ravnica and a founding member of the Gatewatch.",
+"He was compleated during the events of Phyrexia: All Will Be One."
+)
+Gideon = "Gideon Jura, born Kytheon Iora, was a hieromancer and a member of Gatewatch.",
+"Affectionately called Gids by Chandra Nalaar and called Beefslab by Liliana Vess.",
+"although he is one of the most important planeswalkers he is most known for the fact the died",
+"during the war of the spark on ravinca he sacrificied himself so that he would die,",
+"instead of lilliana vess when she broker her demonic contract and went against nicol bolas's orders"
 
-Gideon = ""
+Chandra = "Chandra Nalaar is a human planeswalker from Kaladesh. She is a member of the Gatewatch.",
+"Chandra is a pyromancer knowns for taking out two of the three eldrazi titans in a blaze of fire,",
+"as well as being an ingenius spellcaster even among the planeswalkers.",
+"Although hotheaded she cares deeply for those she calls her friends."
 
-Chandra = ""
+Nissa = "Nissa Revane is a Joraga Zendikari elf and former planeswalker,","who wields magic to muster elves and elementals and amplify their might.",
+"She was a member of the Gatewatch, before she was compleated during the events of Phyrexia: All Will Be One.",
+"At the end of New Phyrexia's Invasion of the Multiverse, she was un-compleated,",
+"and subsequently lost her spark as part of the consequences of the invasion."
 
-Nissa = ""
-
-Liliana = ""
+Liliana = "Liliana Vess is a human planeswalker from Dominaria and is both a master necromancer and a healer.",
+"For a while, she was dead to the Multiverse, her survival known only to select members of the Gatewatch.",
+"Having freed herself from all her demonic contracts as well as from the Onakke Chain Veil,"
+"she relocated to Fiora where she temporarily lived under the alias Ana Iora.",
+"On Arcavios she was originally known as Professor Serafina Onyx, before reclaiming her original name,",
+"and began taking steps to reclaim her ancestral Dominarian home. The Raven Man affectionally calls her Lili."
 
 
 Phyrexians = {"Elesh Norn" : "Mother of Machines, Grand Cenobite, the white Praetor",
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import lore
 
 
 def lore_locater(key_list):
+
     for i in range(len(key_list)):
         if Second_interest == key_list[i]:
 
@@ -11,54 +12,140 @@ def lore_locater(key_list):
             print(lore.key_list[i])
 
 
-
-
-
-User_interest  = input("which group do you wish to know about? Phyrexians Eldrazi or Gatewatch?: ")
-
-
-if User_interest == "Eldrazi":
-    key_list = list(lore.Eldrazi.keys())
-    key_string = ", ".join(key_list)
-    Second_interest = input("which Eldrazi titan do you wish to know about? " + key_string + "? ")
+def lore_locater_extended():
     if Second_interest == "Emrakul":
-        print(lore.Eldrazi["Emrakul"])
         print(lore.Emrakul)
-        answer = input("would you like to know more?(yes/no): ")
-        if answer == "yes":
-            print(lore.Emrakul)
-
-    if Second_interest == "Ulamog":
-        print(lore.Eldrazi["Ulamog"])
 
     if Second_interest == "Kozilek":
-        print(lore.Eldrazi["Kozilek"])
+        print(lore.Kozilek)
 
+    if Second_interest == "Ulamog":
+        print(lore.Ulamog)
 
+    if Second_interest == "Elesh_Norn":
+        print(lore.Elesh_Norn)
 
+    if Second_interest == "Urabrask":
+        print(lore.Urabrask)
 
+    if Second_interest == "Vorinclex":
+        print(lore.Vorinclex)
 
-if User_interest == "Gatewatch":
-    key_list = list(lore.Gatewatch.keys())
-    key_string = ", ".join(key_list)
-    Second_interest = input("which Gatewatch Planeswalker do you wish to know about? " + key_string+ "? ")
+    if Second_interest == "Sheoldred":
+        print(lore.Sheoldred)
     
-    for i in range(len(key_list)):
-        if Second_interest == key_list[i]:
-            print(lore.Gatewatch[key_list[i]])
+    if Second_interest == "Jin_Gitaxis":
+        print(lore.Jin_Gitaxis)
+
+    if Second_interest == "Gix":
+        print(lore.Gix)
+
+    if Second_interest == "Gideon":
+        print(lore.Gideon)
+
+    if Second_interest == "Chandra":
+        print(lore.Chandra)
+
+    if Second_interest == "Nissa":
+        print(lore.Nissa)
+
+    if Second_interest == "Jace":
+        print(lore.Jace)
+
+    if Second_interest == "Liliana":
+        print(lore.Liliana)
+
+    else:
+        print("error occured, please restart")
+
+
+x = True
+
+while x == True:
+    # User_interest  = input("which group do you wish to know about? Phyrexians Eldrazi or Gatewatch?: ")
+    User_interest = "Eldrazi"
+
+    if User_interest == "Eldrazi":
+        details = lore.Eldrazi
+        titans = list(details.keys())
+        key_string = ", ".join(titans)
+        # Second_interest = input("which Eldrazi titan do you wish to know about? " + key_string + "? ")
+        Second_interest= "Emrakul"
+        for titan in titans:
+            if Second_interest == titan:
+                card = details.get(Second_interest).get("card")
+                
+                answer = input("Would you like to know more?(yes/no)")
+
+                if answer == "yes":
+                    lore_locater_extended()
+    
+
+
+
+    if User_interest == "Gatewatch":
+        key_list = list(lore.Gatewatch.keys())
+        key_string = ", ".join(key_list)
+        Second_interest = input("which Gatewatch Planeswalker do you wish to know about? " + key_string+ "? ")
+        
+        for i in range(len(key_list)):
+            if Second_interest == key_list[i]:
+                print(lore.Gatewatch[key_list[i]])
 
 
 
 
 
-if User_interest == "Phyrexians":
-    key_list = list(lore.Phyrexians.keys())
-    key_string = ", ".join(key_list)
-    Second_interest = input("which Phyrexian Praetor do you wish to know about? " + key_string + "? ")
-    for i in range(len(key_list)):
-        if Second_interest == key_list[i]:
-            print(lore(Second_interest[key_list[i]]))
-            Answer = input("Would you like to know more?(yes/no)")
+    if User_interest == "Phyrexians":
+        key_list = list(lore.Phyrexians.keys())
+        key_string = ", ".join(key_list)
+        Second_interest = input("which Phyrexian Praetor do you wish to know about? " + key_string + "? ")
+        for i in range(len(key_list)):
+            if Second_interest == key_list[i]:
+                print(lore.Phyrexians[key_list])
+                Answer = input("Would you like to know more?(yes/no)")
 
-            print(lore(Second_interest))
+                print(lore(Second_interest))
+    
+    while x == True:
+        cycler = input("would you like to know about a different group?(yes/no): ")
+        if cycler == "yes":
+           break
+        elif cycler == "no":
+            x = False
+        else:
+            print("error please only answer yes or no")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
